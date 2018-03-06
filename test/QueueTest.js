52432aa3150eab18a58ba5783639d01e111dcbd9
chocojazz <jasmine_singh@aol.com>
2018-03-06 13:44:53 -0800

    q tests

--- test/QueueTest.js
+++ test/QueueTest.js
@@ -16,7 +16,14 @@ contract('QueueTest', function(accounts) {
 	describe('Your string here', function() {
 		it("Queue has fixed size of 5", async function() {
 			let qSize = q.qsize();
-			assert.equal(qSize, 5, "set correct");
+			assert.equal(qSize, 3456, "set correct");
+		});
+
+		it("Front is kicked out after time limit", async function() {
+			let exampleAddress = "0x123f681646d4a755815f9cb19e1acc8565a0c2ac"
+			q.enqueue(exampleAddress);
+
+			
 		});
 	});
 });
