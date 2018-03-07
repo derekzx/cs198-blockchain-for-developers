5a66fde1c493dfe172cd09577bc167ec27b8cdc5
Ponzi-Coin <chinzhixian@gmail.com>
2018-03-06 17:01:02 -0800

    Test Templates Ready

--- test/QueueTest.js
+++ test/QueueTest.js
@@ -6,25 +6,30 @@ const Queue = artifacts.require("./Queue.sol");
 // YOUR CODE HERE
 
 contract('QueueTest', function(accounts) {
+	const args = {_timeLimit: 1000};
+	let q;
 
 	/* Do something before every `describe` method */
 	beforeEach(async function() {
 		//YOUR CODE HERE
-		let timeLimit = 1000;
-		q = await Queue.new(timeLimit);
+		q = await Queue.new();
+		q.setTimeLimit(args._timeLimit).call();
+		//TypeError: q.setTimeLimit(...).call is not a function
 	});
 	
 	describe('~Queue Works~', function () {
 		describe('Queue Size Maintenance', function() {
 			it("Queue has fixed size of 5", async function() {
+				//YOUR CODE HERE
 				let qSize = q.qsize();
 				assert.equal(qSize, 3456, "set correct");
 			});
 
 			it("Front of queue out after time limit is reached", async function() {
+				//YOUR CODE HERE
 				let exampleAddress = "0x123f681646d4a755815f9cb19e1acc8565a0c2ac"
 				q.enqueue(exampleAddress);
-				//?? this doesnt really work though
+				
 				
 			});
 		});
